a89106a8fef8ab0b912103e50409ff4b1a22f7e6
rive3 <rive3@workstation>
2020-09-06 22:20:27 +0900

    update

--- sshkey_setup_win.sh
+++ sshkey_setup_win.sh
@@ -1,44 +1,49 @@
 #!/bin/bash
 
 vm_addr=$1
-vm_user=$2
-vm_passwd=$3
+vm_passwd=$2
+mstnode_account=$3
+default_user=root
 
-ssh_dir="C:\Users\\${vm_user}\.ssh"
-dir_exist=`sshpass -p ${vm_passwd} ssh ${vm_user}@${vm_addr} "powershell Test-Path ${ssh_dir}"`
+ssh_dir="C:\Users\\${default_user}\.ssh"
+
+# clear old ssh keys if exist
+ssh-keygen -f "/home/${mstnode_account}/.ssh/known_hosts" -R ${vm_addr} > /dev/null
+
+dir_exist=`sshpass -p ${vm_passwd} ssh ${default_user}@${vm_addr} "powershell Test-Path ${ssh_dir}"`
 if [ $(echo "$dir_exist" | grep -e 'True') ]; then
     echo "## .ssh directory exist"
 else
     echo "## .ssh directory not exist. make .ssh"
-    sshpass -p ${vm_passwd} ssh ${vm_user}@${vm_addr} "mkdir ${ssh_dir}"
+    sshpass -p ${vm_passwd} ssh ${default_user}@${vm_addr} "mkdir ${ssh_dir}"
 fi
 
 
-dir_exist=`sshpass -p ${vm_passwd} ssh ${vm_user}@${vm_addr} "powershell Test-Path ${ssh_dir}\authorized_keys"`
+dir_exist=`sshpass -p ${vm_passwd} ssh ${default_user}@${vm_addr} "powershell Test-Path ${ssh_dir}\authorized_keys"`
 if [ $(echo "$dir_exist" | grep -e 'True') ]; then
     echo "## authorized_keys file exist"
 else
     echo "## authorized_keys file not exist. make authorized_keys"
-    sshpass -p ${vm_passwd} ssh ${vm_user}@${vm_addr} "type nul > ${ssh_dir}\authorized_keys"
+    sshpass -p ${vm_passwd} ssh ${default_user}@${vm_addr} "type nul > ${ssh_dir}\authorized_keys"
 fi
 
 
 echo "## add sshkey in authorized_keys"
 sshkey=`cat ~/.ssh/id_rsa.pub`
-sshpass -p ${vm_passwd} ssh ${vm_user}@${vm_addr} "echo ${sshkey} >> ${ssh_dir}\authorized_keys"
+sshpass -p ${vm_passwd} ssh ${default_user}@${vm_addr} "echo ${sshkey} >> ${ssh_dir}\authorized_keys"
 
 
 echo "## create change acl script"
-sshpass -p ${vm_passwd} ssh ${vm_user}@${vm_addr} "type nul > ${ssh_dir}\create_ch_acl.ps1"
-sshpass -p ${vm_passwd} ssh ${vm_user}@${vm_addr} "echo \$authorizedKeyPath = \"${ssh_dir}\\authorized_keys\" >> ${ssh_dir}\create_ch_acl.ps1"
-sshpass -p ${vm_passwd} ssh ${vm_user}@${vm_addr} "echo \$acl = Get-Acl \$authorizedKeyPath >> ${ssh_dir}\create_ch_acl.ps1"
-sshpass -p ${vm_passwd} ssh ${vm_user}@${vm_addr} "echo \$ar = New-Object System.Security.AccessControl.FileSystemAccessRule(\"NT Service\\sshd\", \"Read\", \"Allow\") >> ${ssh_dir}\\create_ch_acl.ps1"
-sshpass -p ${vm_passwd} ssh ${vm_user}@${vm_addr} "echo \$acl.SetAccessRule(\$ar) >> ${ssh_dir}\\create_ch_acl.ps1"
-sshpass -p ${vm_passwd} ssh ${vm_user}@${vm_addr} "echo Set-Acl \$authorizedKeyPath \$acl >> ${ssh_dir}\\create_ch_acl.ps1"
+sshpass -p ${vm_passwd} ssh ${default_user}@${vm_addr} "type nul > ${ssh_dir}\create_ch_acl.ps1"
+sshpass -p ${vm_passwd} ssh ${default_user}@${vm_addr} "echo \$authorizedKeyPath = \"${ssh_dir}\\authorized_keys\" >> ${ssh_dir}\create_ch_acl.ps1"
+sshpass -p ${vm_passwd} ssh ${default_user}@${vm_addr} "echo \$acl = Get-Acl \$authorizedKeyPath >> ${ssh_dir}\create_ch_acl.ps1"
+sshpass -p ${vm_passwd} ssh ${default_user}@${vm_addr} "echo \$ar = New-Object System.Security.AccessControl.FileSystemAccessRule(\"NT Service\\sshd\", \"Read\", \"Allow\") >> ${ssh_dir}\\create_ch_acl.ps1"
+sshpass -p ${vm_passwd} ssh ${default_user}@${vm_addr} "echo \$acl.SetAccessRule(\$ar) >> ${ssh_dir}\\create_ch_acl.ps1"
+sshpass -p ${vm_passwd} ssh ${default_user}@${vm_addr} "echo Set-Acl \$authorizedKeyPath \$acl >> ${ssh_dir}\\create_ch_acl.ps1"
 
 
 echo "## exe change acl script"
-sshpass -p ${vm_passwd} ssh ${vm_user}@${vm_addr} "powershell ${ssh_dir}\create_ch_acl.ps1"
+sshpass -p ${vm_passwd} ssh ${default_user}@${vm_addr} "powershell ${ssh_dir}\create_ch_acl.ps1"
 
 echo "## delete acl script"
-sshpass -p ${vm_passwd} ssh ${vm_user}@${vm_addr} "del /Q ${ssh_dir}\create_ch_acl.ps1"
+sshpass -p ${vm_passwd} ssh ${default_user}@${vm_addr} "del /Q ${ssh_dir}\create_ch_acl.ps1"
